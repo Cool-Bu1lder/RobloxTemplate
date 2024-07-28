335405064061232350cba004e4ea959a650ab4c9
Cool-Bu1lder <Cool-Bu1lder@users.noreply.github.com>
2024-07-28 13:03:26 +0000

    deploy: 420f22c9f531763e95593e0687270628acddaf8e

--- assets/js/418666d7.be627268.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[50],{7636:s=>{s.exports=JSON.parse('[{"type":"link","href":"/api/MyFirstClass","label":"\u200bMy\u200bFirst\u200bClass"}]')}}]);
--- assets/js/a3e13737.f66d58e5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[250],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/Cool-Bu1lder/RobloxTemplate/blob/master","baseUrl":"/RobloxTemplate/","classOrder":[],"apiCategories":[]}')}}]);
