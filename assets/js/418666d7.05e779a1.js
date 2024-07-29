6ec9cd73e511692a601a90e4b22c47f3d1d70349
Cool-Bu1lder <Cool-Bu1lder@users.noreply.github.com>
2024-07-29 08:51:38 +0000

    deploy: edda97df74593e13c6dd6a91299a9109637f36ef

---
+++ assets/js/418666d7.05e779a1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[50],{7636:s=>{s.exports=JSON.parse('[{"type":"link","href":"/api/MyFirstClass","label":"\u200bMy\u200bFirst\u200bClass"},{"type":"link","href":"/api/PlayerBuildSlotsDOA","label":"\u200bPlayer\u200bBuild\u200bSlots\u200bD\u200bO\u200bA"}]')}}]);
